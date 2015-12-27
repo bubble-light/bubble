7d1969943462eeeabede2982e0c26c50faa97815
shiwen_xiao <xiaosw@msn.cn>
2015-12-27 22:00:32 +0800

    Controller支持jsonp配置修改

--- bubble-application/src/main/java/net/bubble/application/framework/configuration/BubbleApplicationConfiguration.java
+++ bubble-application/src/main/java/net/bubble/application/framework/configuration/BubbleApplicationConfiguration.java
@@ -1,17 +1,14 @@
 /**
  * Copyright [2015-2017] [https://github.com/bubble-light/]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 package net.bubble.application.framework.configuration;
 
@@ -20,14 +17,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
 /**
  * @author shiwen_xiao<xiaosw@msn.cn>
  * @since 2015年2月6日
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages={"net.bubble.application.**.controller", "net.bubble.application.**.service"})
-@ImportResource({"classpath*:config/applicationContext.xml", "classpath*:config/persistence/persistence.xml"})
+@ComponentScan(basePackages = { "net.bubble.application.**.controller", "net.bubble.application.**.service", "net.bubble.application.**.advice" })
+@ImportResource({ "classpath*:config/applicationContext.xml", "classpath*:config/persistence/persistence.xml", "classpath*:config/app/application.xml" })
 public class BubbleApplicationConfiguration {
 }
